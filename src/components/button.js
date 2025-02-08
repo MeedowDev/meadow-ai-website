f05c3dd05d40639485c8ab7e0d2ff76c9b05a073
victorcodebase <victorkithinji@outlook.com>
2025-02-08 23:21:36 +0300

    landing section

--- src/components/button.js
+++ src/components/button.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./button.css";
 
-export default function Button({ text, color, link }) {
-	return (
-		<div className="button">
-			<a href={link} style={{ backgroundColor: color }} className="button">
-				{text}
-			</a>
-		</div>
-	);
+export default function Button({ text, color, link, text_color = "black" }) {
+    return (
+        <div className="button">
+            <a href={link} style={{ backgroundColor: color, color: text_color }} className="button">
+                {text}
+            </a>
+        </div>
+    );
 }
--- src/components/landing.js
+++ src/components/landing.js
@@ -3,6 +3,8 @@ import React from "react";
 import "./landing.css";
 import Button from "./button";
 import tractorImage from "../assets/hero.png"; 
+import image01 from "../assets/img-01.png";
+import image02 from "../assets/img-02.png";
 
 const Landing = () => {
 	return (
@@ -16,16 +18,43 @@ const Landing = () => {
 					<Button text="Get Started" color="White" link=""></Button>
 				</div>
 
-                {/* Data driven agriculture */}
+				{/* Data driven agriculture */}
 			</div>
-            <div className="landing-information">
-                <div className="landing-pre">
-                    <div className="contribute-to-project"></div>
-                    <div className="down"></div>
-                    <div className="data-driven-agriculture"></div>
-                </div>
-                <div className="information-card"></div>
-            </div>
+			<div className="landing-information">
+				<div className="landing-pre">
+					<div className="contribute-to-project"></div>
+					<div className="down"></div>
+					<div className="data-driven-agriculture"></div>
+				</div>
+				<div className="information-card">
+					<div className="content-wrapper">
+						<div className="image-container">
+							<img src={image02} alt="Farmer with oranges" />
+						</div>
+						<div className="text-container">
+							<div className="small-image">
+								<img src={image01} alt="Gardening" />
+							</div>
+							<h2>
+								Introducing <span className="highlight-green">Data Driven</span> agriculture.
+							</h2>
+							<p>
+								Greetings from Dosner Organic Farms. We distribute only organic herbs and produce directly to
+								consumers.
+							</p>
+							<ul className="features-list">
+								<li>✔️ Gourmet Mushrooms</li>
+								<li>✔️ Natural Healthy Products</li>
+								<li>✔️ Lavender Farming</li>
+								<li>✔️ Best Quality Standards</li>
+								<li>✔️ Fertilizer Distribution</li>
+								<li>✔️ Organic Fertilizer</li>
+							</ul>
+							<Button text="Know More" color="#5B8C51" link="" text_color="white"></Button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
